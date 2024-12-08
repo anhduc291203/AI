cd47333e62c70bc28c6b03f030ed8b18c5c03376
Hai <duchaisub01@gmail.com>
2024-12-08 21:15:54 +0700

    Q4

--- reinforcement/qlearningAgents.py
+++ reinforcement/qlearningAgents.py
@@ -113,7 +113,14 @@ class QLearningAgent(ReinforcementAgent):
         legalActions = self.getLegalActions(state)
         action = None
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # util.raiseNotDefined()
+        if len(legalActions) == 0:
+            return None
+
+        if util.flipCoin(self.epsilon):
+            action = random.choice(legalActions)
+        else:
+            action = self.computeActionFromQValues(state)
 
         return action
 
